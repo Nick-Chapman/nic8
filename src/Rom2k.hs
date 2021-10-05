81c3e110e4a02a3885b00673d408c7e14de41fb0
Nick Chapman <nic@amadido.co.uk>
2021-10-05 23:46:01 +0100

    compute rather than hard-code the seg-encoding for decimal digits

--- src/Rom2k.hs
+++ src/Rom2k.hs
@@ -3,6 +3,7 @@ module Rom2k (generateAll) where
 import Prelude hiding (take)
 import qualified Data.ByteString as BS
 import Data.Word8 (Word8)
+import Data.Bits (shiftL)
 
 take :: Int -> [a] -> [a]
 take n = loop (0::Int) n
@@ -57,15 +58,53 @@ decimalAndHexLED = concat
   , concat (map (replicate 16) hdigs)
   ]
 
+erasedPage :: [Word8]
+erasedPage = replicate 256 0xff
+
 ddigs :: [Word8]
-ddigs = take 10 hdigs
+--ddigs = take 10 hdigs
+ddigs = [ encodeSegs (segs i) | i <- [0..9] ]
 
 hdigs :: [Word8]
 hdigs = [0x3f, 0x0c, 0x5b, 0x5e, 0x6c, 0x76, 0x77, 0x1c, 0x7f, 0x7c
         ,1,2,3,4,5,6] -- TODO: determine LED code for hex values: a,b,c,d,e,f
 
-erasedPage :: [Word8]
-erasedPage = replicate 256 0xff
+data Seg = A | B | C | D | E | F | G deriving Eq
+{-
+                      A
+                    F   B
+                      G
+                    E   C
+                      D
+-}
+segs :: Int -> [Seg]
+segs = \case
+  0 -> [ A, B, C, D, E, F    ]
+  1 -> [    B, C             ]
+  2 -> [ A, B,    D, E,    G ]
+  3 -> [ A, B, C, D,       G ]
+  4 -> [    B, C,       F, G ]
+  5 -> [ A,    C, D,    F, G ]
+  6 -> [ A,    C, D, E, F, G ]
+  7 -> [ A, B, C             ]
+  8 -> [ A, B, C, D, E, F, G ]
+  9 -> [ A, B, C,       F, G ]
+  n ->
+    error (show ("segs",n))
+
+encodeSegs :: [Seg] -> Word8
+encodeSegs segs = sum [ 1 `shiftL` segBit seg | seg <- segs ]
+
+segBit :: Seg -> Int
+segBit seg = the [ n | (x,n) <- bitMap, x == seg]
+  where
+    bitMap = zip order [0..]
+    order = [E,D,C,B,A,F,G]
+
+the :: Show a => [a] -> a
+the = \case
+  [x] -> x
+  xs -> error (show ("the",xs))
 
 genRom2k :: String -> [Word8] -> IO ()
 genRom2k name bytes = do
