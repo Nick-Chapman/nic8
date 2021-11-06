fda54737134c9a6d0c39e3940d6b7fdec4a4b3f7
Nick Chapman <nic@amadido.co.uk>
2021-11-06 11:23:44 +0000

    lcd: return_home + comments

--- 6502/lib/lcd.s
+++ 6502/lib/lcd.s
@@ -1,12 +1,17 @@
 
 ;;; routines to initialize and print to the LCD screen, in 4 bit mode
 
-;;; LCD control bits on port A
+;;; LCD control bits on port B
 E  = %00001000
 RW = %00000100                  ; 1-read
 RS = %00000010                  ; 1-data
 
 NOT_E = %11110111
+
+;;; PortB bits: ddddEccX -- 4 data bits; enable bit; 2 control bits
+
+;;; The final bit is not for the LCD. It's the active-low enable for
+;;; the sound chip. We make sure it is always set high here.
 
 SOUND_CLOCK_INACTIVE = 1
 
@@ -38,11 +43,19 @@ print_char:
     jsr send_portB                   ; E lo again
     rts
 
-clear_display:
+clear_display: ;(0000 0001)
     jsr wait_lcd
     lda #%00000000
     jsr send_lcd_command_nibble
     lda #%00010000
+    jsr send_lcd_command_nibble
+    rts
+
+return_home: ;(0000 001x)
+    jsr wait_lcd
+    lda #%00000000
+    jsr send_lcd_command_nibble
+    lda #%00100000
     jsr send_lcd_command_nibble
     rts
 
@@ -54,19 +67,19 @@ init_display:
     lda #%00100000
     jsr send_lcd_command_nibble
 
-    ;; function set: 4 bit; 2 lines, 5x8
+    ;; function set: 4 bit(L=0); 2 lines(N=1), 5x8 (001L NFxx)
     jsr wait_lcd
     lda #%00100000
     jsr send_lcd_command_nibble
     lda #%10000000
     jsr send_lcd_command_nibble
-    ;; turn on display; no cursor; no blinking
+    ;; turn on display(D=1); no cursor(C=0); no blinking(B=0) (0000 1DCB)
     jsr wait_lcd
     lda #%00000000
     jsr send_lcd_command_nibble
     lda #%11000000
     jsr send_lcd_command_nibble
-    ;; entry mode: increment; no shift
+    ;; entry mode: increment(I=1); no shift(S=0) (0000 01IS)
     jsr wait_lcd
     lda #%00000000
     jsr send_lcd_command_nibble
