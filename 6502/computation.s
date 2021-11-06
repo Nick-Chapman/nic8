8b56945d4ffa7b3ca557d253749478c10e57a5f2
Nick Chapman <nic@amadido.co.uk>
2021-11-06 16:46:57 +0000

    count up; print from in-mem screen 20/s

--- 6502/computation.s
+++ 6502/computation.s
@@ -1,22 +1,22 @@
 
 ;;; Play with (forth-based) ideas to structure computations:
-;;; - 0 basic 16bit leaf ops (+, inc) which can be combined
-;;; - 1 use of data-stack for arguments and results
-;;; - 2 forth style composition
+;;; - 0 basic 16bit leaf ops (+, inc) which can be combined DONE
+;;; - 1 use of data-stack for arguments and results DONE
+;;; - 2 forth style composition DONE
 ;;; - 3 maintain own return stack, so...
 ;;; - 4 task switching (each with own pair of stacks)
 ;;; - 5 explore co-operative vs pre-emptive task switching
 ;;; Try out assembly macros
 ;;; Idea for computations:
-;;; - counting
+;;; - counting DONE
 ;;; - collatz
 ;;; - slow fib calculation
 ;;; - prime generation
-;;; - binary2decimal
+;;; - binary2decimal DONE
 ;;; - multiplication
 ;;; - ops for implementing a clock
 ;;; Beter LCD abstraction
-;;; - perhaps manipulate screen-contents in memory
+;;; - perhaps manipulate screen-contents in memory DONE
 ;;; - with sep thread which keeps LCD up to date with mem
 
     .org $fffc
@@ -42,7 +42,8 @@ reset_main:
     jsr init_timer
     jsr init_display
     jsr clear_display
-    jsr even_simpler_example
+    jsr init_screen
+    jsr example
 spin:
     jmp spin
 
@@ -51,117 +52,68 @@ init_via:
     sta DDRB
     rts
 
-
-even_simpler_example:
-    jsr init_screen
-    jsr sleep1
-    jsr update_pause
-    lda #'1'
-    jsr putchar
-    jsr update_pause
-    lda #'2'
-    jsr putchar
-    jsr update_pause
-    lda #'3'
-    jsr putchar
-    jsr update_pause
-    lda #'x'
-    jsr putchar
-    jsr putchar
-    jsr putchar
-    jsr putchar
-    jsr putchar
-    jsr putchar
-    jsr putchar
-    jsr putchar
-    jsr putchar
-    jsr putchar
-    jsr update_pause
-    lda #'4'
-    jsr putchar
-    jsr update_pause
-    lda #'5'
-    jsr putchar
-    jsr update_pause
-    lda #'6'
-    jsr putchar
-    jsr update_pause
-    lda #'7'
-    jsr putchar
-    jsr update_pause
-    lda #'8'
-    jsr putchar
-    jsr update_pause
-    rts
-
-update_pause:
-    ;jsr clear_display
-    jsr print_screen
-    jsr sleep1
-    rts
-
-simple_example: ; just while testing asyn print
-    jsr return_home
-    lda #<789
-    ldx #>789
-    jsr print_word_ax_in_decimal
-    jsr print_dot
-
-    jsr sleep1
-    jsr return_home
-    lda #<1234
-    ldx #>1234
-    jsr print_word_ax_in_decimal
-    jsr print_dot
-
-    jsr sleep1
-    jsr return_home
-    lda #42
-    ldx #0
-    jsr print_word_ax_in_decimal
-    jsr print_dot
-    rts
-
-
-sleep1:
-    pha
-    lda #100
-    jsr sleep
-    pla
-    rts
-
 ;;;--------------------
 ;;; example which manipulates 16bit number in mem
 ;;; by dispatching to stack based ops
 example:
     jsr init_number
     jsr init_ds_stack
+    jsr print_screen_now
 example_loop:
-    jsr print_number_dec
-    jsr print_dot
+    jsr put_number_dec
+    jsr put_dot
+    jsr print_screen_when_time
     jsr push_number
-    jsr ds_decrement
+    jsr ds_increment
     jsr pull_number
     jmp example_loop
 
+next_screen_print = $33
+print_screen_when_time:
+    lda next_screen_print
+    sec
+    sbc ticks
+    beq print_screen_now
+    rts
+print_screen_now:
+    jsr print_screen
+    lda ticks
+    clc
+    adc #5 ; 20 times/sec
+    sta next_screen_print
+    rts
+
+sleep:
+    clc
+    adc ticks
+    pha ; goal ticks
+sleep_wait:
+    sec
+    pla
+    pha
+    sbc ticks
+    bne sleep_wait
+    pla
+    rts
+
 init_number:
-    lda #100
+    lda #0
     sta g_number
     lda #0
     sta g_number + 1
     rts
 
-print_number_dec: ; print 1-5 decimal digits for 16 bit number
+put_number_dec: ; print 1-5 decimal digits for 16 bit number
     jsr return_home
     lda g_number
     ldx g_number + 1
-    jsr print_word_ax_in_decimal
+    jsr put_word_ax_in_decimal
     rts
 
-print_dot:
+put_dot:
     pha ; make sure this debug routine changes no registers!
     lda #'.'
-    jsr print_char
+    jsr putchar
     pla
     rts
 
@@ -255,22 +207,22 @@ ds_add: ; (A B -- A+B)
 
 ;;;--------------------
 ;;; sleep for N (in acc) 1/100s
-sleep:
-    clc
-    adc ticks
-    pha ; goal ticks
-sleep_wait:
-    sec
-    pla
-    pha
-    sbc ticks
-    bne sleep_wait
-    pla
-    rts
+;; sleep:
+;;     clc
+;;     adc ticks
+;;     pha ; goal ticks
+;; sleep_wait:
+;;     sec
+;;     pla
+;;     pha
+;;     sbc ticks
+;;     bne sleep_wait
+;;     pla
+;;     rts
 
 ;;;--------------------
 ;;; print word passed in a/x as 1-5 digit decimal number
-print_word_ax_in_decimal:
+put_word_ax_in_decimal:
     sta g_divisor ;lo
     stx g_divisor + 1 ;hi
     lda #0
@@ -310,11 +262,11 @@ ignore_result:
     lda g_divisor
     ora g_divisor + 1
     bne each_digit
-print_from_stack:
+put_from_stack:
     pla
     beq done
-    jsr print_char
-    jmp print_from_stack
+    jsr putchar
+    jmp put_from_stack
 done:
     rts
 
@@ -324,44 +276,31 @@ done:
 g_screen = $200 ; 32 bytes
 g_screen_pointer = $220
 
+;; put* functions write to the in-memory screen
 putchar:
-    ;jsr print_char
     ldx g_screen_pointer
     sta g_screen,x
     inc g_screen_pointer
     rts
 
 return_home:
-    ;jsr lcd_return_home
     lda #0
     sta g_screen_pointer
     rts
 
 init_screen:
     jsr return_home
-    ;; copy patten into screen array: a..z, 3 spaces, 2 Xs, !
     ldx #0
+    lda #' '
 each_pat_char:
-    txa
-    clc
-    adc #'a'
     sta g_screen,x
     inx
     sec
-    cpx #26
+    cpx #32
     bne each_pat_char
-    lda #' ' ; space
-    sta g_screen+26
-    sta g_screen+27
-    sta g_screen+28
-    lda #'X'
-    sta g_screen+29
-    sta g_screen+30
-    lda #'!'
-    sta g_screen+31 ; bang in last place
     rts
 
-
+;; print_* functions write to the underlying lcd
 print_screen:
     ;; copy screen to lcd
     jsr lcd_return_home
