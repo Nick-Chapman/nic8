9fa7671ec61d8659942287e0462da5faee6b7508
Nick Chapman <nic@amadido.co.uk>
2021-12-01 22:07:56 +0000

    progress on primes

--- 6502/app/run-primes.s
+++ 6502/app/run-primes.s
@@ -17,17 +17,37 @@
     include "print.s"
     include "panic.s"
     include "macs.s"
+    include "gc.s"
     include "primes.s"
 
+screen_flush_when_time: ; TODO
+    rts
+
+pause:
+    pha
+    lda #1
+    jsr sleep_blocking
+    pla
+    rts
+
 ;;; bytes
+heap_end_page = $30
+n_bytes = $31
 g_ticks = $32
+gc_debug = $33
 g_selected_screen = $34
 
+;;; words
 hp = $40
 fp = $42
 cp = $44
 clo = $46
 ev = $48
+lw = $4a
+temp = $4c
+space_switcher = $4c
+heap_start = $4e
+gc_count = $50
 
 g_divisor = $54 ; decimal.s
 g_mod10 = $56 ; decimal.s
@@ -37,18 +57,86 @@ g_screen_pointers = $80
 ;;; buffers
 g_screens = $200 ; 4*32 bytes
 
+;;; ----------------------------------------------------------------------
+;;; show/test divides
+;; show_divides:
+;;     print_char ','
+;;     print_decimal_word 1
+;;     print_char '/'
+;;     print_decimal_word 3
+;;     screen_flush_selected
 
-test_divides: macro A, B
+;;     copy16_literal_to_var after_divides.static_closure, 5
+;;     copy16_literal_to_var divides.static_closure, fp
+;;     enter_fp
+
+;; after_divides:
+;; .code:
+;;     print_char '='
+;;     print_decimal_byte 1
+;;     screen_flush_selected
+;;     jsr pause
+;;     rts
+;; .static_closure:
+;;     word .code
+
+;; test_divides: macro A, B
+;;     lda #<\A
+;;     sta 1
+;;     lda #>\A
+;;     sta 2
+;;     lda #<\B
+;;     sta 3
+;;     lda #>\B
+;;     sta 4
+;;     jsr show_divides
+;; endmacro
+
+;;; ----------------------------------------------------------------------
+;;; show/test candidate
+
+show_candidate:
+    ;print_char ','
+    print_decimal_word 1
+    screen_flush_selected
+
+    copy16_literal_to_var after_candidate.static_closure, 5
+    copy16_literal_to_var candidate.static_closure, fp
+    enter_fp
+
+
+print_bool:
+    beq .dot
+    print_char 'x'
+    rts
+.dot:
+    print_char '.'
+    rts
+
+;;; should there be the roots/evac/scav table here? -yes, but the easy static thing
+after_candidate: ; same as after divides - just displays a bool
+.code:
+    ;print_char '='
+;print_decimal_byte 1
+    lda 1
+    jsr print_bool
+    screen_flush_selected
+    jsr pause
+    rts
+.static_closure:
+    word .code
+
+test_candidate: macro A, PS
     lda #<\A
     sta 1
     lda #>\A
     sta 2
-    lda #<\B
-    sta 3
-    lda #>\B
-    sta 4
-    jsr show_divides
+    copy16_literal_to_var \PS, 3 ; nil
+    jsr show_candidate
 endmacro
+
+;;; ----------------------------------------------------------------------
+;;; main
 
 reset_main:
     ldx #$ff
@@ -58,52 +146,73 @@ reset_main:
     jsr init_lcd
     jsr lcd_clear_display
     jsr init_screen
+    jsr init_gc
 
-    test_divides 0, 3
-    test_divides 1, 3
-    test_divides 2, 3
-    test_divides 3, 3
-    test_divides 4, 3
-    test_divides 5, 3
-    test_divides 6, 3
-    test_divides 7, 3
+    ;; test_divides 0, 3
+    ;; test_divides 1, 3
+    ;; test_divides 2, 3
+    ;; test_divides 3, 3
+    ;; test_divides 4, 3
+    ;; test_divides 5, 3
+    ;; test_divides 6, 3
+    ;; test_divides 7, 3
 
-    test_divides 2000, 399
-    test_divides 2000, 400
-    test_divides 2000, 401
+    ;; test_divides 2000, 399
+    ;; test_divides 2000, 400
+    ;; test_divides 2000, 401
 
-    test_divides 10000, 5
-    test_divides 10001, 5
+    ;; test_divides 10000, 5
+    ;; test_divides 10001, 5
 
+    ;; test_divides 99, 33
+
+    ;; test_candidate 6, nil_cell_i16.static_closure
+    ;; test_candidate 7, nil_cell_i16.static_closure
+
+    ;; test_candidate 6, list_32
+    ;; test_candidate 7, list_32
+    ;; test_candidate 8, list_32
+    ;; test_candidate 9, list_32
+
+    ldy #0
+    ldx #2
+.loop:
+    phx
+    phy
+    stx 1 ; lo
+    sty 2 ; hi
+    copy16_literal_to_var list_7532, 3
+    jsr show_candidate
+    ply
+    plx
+    inx
+    bne .loop
+    iny
+    bne .loop
+    ;; dont expect to everer get here!
     print_char "$"
     screen_flush_selected
 spin:
     jmp spin
 
-show_divides:
-    print_char ','
-    print_decimal_word 1
-    print_char '/'
-    print_decimal_word 3
-    screen_flush_selected
 
-    copy16_literal_to_var after_divides.static_closure, 5
-    copy16_literal_to_var divides.static_closure, fp
-    enter_fp
+;;; static lists
+list_2:
+    word cons_cell_i16.code
+    word 2
+    word nil_cell_i16.static_closure
 
-after_divides:
-.code:
-    print_char '='
-    print_decimal_byte 1
-    screen_flush_selected
-    jsr pause
-    rts
-.static_closure:
-    word .code
+list_32:
+    word cons_cell_i16.code
+    word 3
+    word list_2
 
-pause:
-    pha
-    lda #100
-    jsr sleep_blocking
-    pla
-    rts
+list_532:
+    word cons_cell_i16.code
+    word 5
+    word list_32
+
+list_7532:
+    word cons_cell_i16.code
+    word 7
+    word list_532
--- 6502/lib/primes.s
+++ 6502/lib/primes.s
@@ -1,4 +1,132 @@
 
+
+debug: macro C
+    print_char \C
+    screen_flush_selected
+endmacro
+
+debug_hex_word: macro L
+    print_hex_word \L
+    screen_flush_selected
+endmacro
+
+debug_hex_byte: macro L
+    print_hex_byte \L
+    screen_flush_selected
+endmacro
+
+debug_decimal_word: macro L
+    print_decimal_word \L
+    screen_flush_selected
+endmacro
+
+;;; ----------------------------------------------------------------------
+;;; extra GC macros
+
+no_evacuate_because_static: macro
+    copy_word ev, clo ;identity because static
+    rts
+endmacro
+
+impossible_scavenge_because_static: macro
+    panic 'S'
+endmacro
+
+impossible_roots: macro
+    panic 'R'
+endmacro
+
+;;; ----------------------------------------------------------------------
+;;; general array access
+copy_word_from_array: macro A, N, L ; A[N] -> L
+    ldy #\N
+    lda (\A),y
+    sta \L
+    ldy #(\N + 1)
+    lda (\A),y
+    sta \L + 1
+endmacro
+
+copy_word_from_array0: macro A, L ; A[0] -> L
+    lda (\A)
+    sta \L
+    ldy #1
+    lda (\A),y
+    sta \L + 1
+endmacro
+
+load_byte_from_array: macro A, N
+    ldy #\N
+    lda (\A),y
+endmacro
+
+;;; ----------------------------------------------------------------------
+;;; push/pull macros
+
+push_word_immediate: macro LIT
+    lda #>\LIT
+    pha
+    lda #<\LIT
+    pha
+endmacro
+
+;;; ----------------------------------------------------------------------
+;;; cons-cell of 16-bit numbers
+;;; (we need to know the element's size & that it's not a heap value)
+;;;
+;;; [. . iL iH tailL tailH]
+cons_cell_i16:
+    word .roots, .evac, .scav
+.code:
+    ;debug 'C'
+    ;; \n c -> c i tail
+    pla ; cL
+    sta temp
+    pla ; cH
+    sta temp + 1
+    pla ; nL
+    pla ; nH
+    ;; setup head/tail
+    load_byte_from_array fp, 3
+    pha ; iH
+    load_byte_from_array fp, 2
+    pha ; iL
+    load_byte_from_array fp, 5
+    pha ; tailH
+    load_byte_from_array fp, 4
+    pha ; tailL
+    jmp (temp)
+.roots:
+    impossible_roots ; because a cons-cell is not a proper closure; we dont 'enter' it
+.evac:
+    evacuate 6 ; dispatch code; head-i16; tail
+.scav
+    scavenge_cell_at 4 ; tail
+    scavenge_done 6
+
+
+nil_cell_i16:
+    word .roots, .evac, .scav
+.code:
+    ;debug 'N'
+    pla ; cL
+    pla ; cH
+    pla ; nL
+    sta temp
+    pla ; nH
+    sta temp + 1
+    jmp (temp)
+.roots:
+    impossible_roots
+.evac:
+    no_evacuate_because_static
+.scav:
+    impossible_scavenge_because_static
+.static_closure:
+    word .code
+
+
+;;; ----------------------------------------------------------------------
 ;;; Number args/locals from 1 (because of plan to have fp as 0)
 
 
@@ -14,26 +142,6 @@ endmacro
 
 True = 1
 False = 0
-
-debug: macro C
-    print_char \C
-    screen_flush_selected
-endmacro
-
-debug_hex_word: macro L
-    print_hex_word \L
-    screen_flush_selected
-endmacro
-
-
-no_evacuate_because_static: macro
-    copy_word ev, clo ;identity because static
-    rts
-endmacro
-
-impossible_scavenge_because_static: macro
-    panic 'S'
-endmacro
 
 ;; primes:
 ;; .code:
@@ -55,55 +163,149 @@ endmacro
 ;;       False ->
 ;;         search (i+1) ps
 
-;; candidate :: Int -> List Int -> (Bool -> r) -> r
-;; candidate i ps k = do
-;;   let kNil = make_kNil (k)
-;;   let kCons = make_kCons (i,k)
-;;   match ps kNil kCons
+;;; TODO: search
 
-;; make_kNil :: (Bool -> r) -> (() -> r)
-;; make_kNil (k) = \() -> k True
-
-;; make_kCons ::(Int,Bool -> r) -> Int -> List Int -> r
-;; make_kCons (i,k) = \p ps -> do
-;;   let k1 = make_candidate_cont (i,ps,k)
-;;   divides i p k1
-
-;; make_candidate_cont :: (Int,List Int,(Bool -> r)) -> Bool -> r
-;; make_candidate_cont (i,ps,k) = \b -> if b then k False else candidate i ps k
-
-;;; divides :: Int -> Int -> (Bool -> r) -> r
-;;; divides i p k =
-;;;   if i == 0 then k True else let i' = i-p in if i' < 0 then k False else divides i' p k
-
-;;; [] il ih pl ph kl kh
-divides:
+;;; candidate :: Int -> List Int -> (Bool -> r) -> r
+;;; candidate i ps k = do
+;;;   let nil = \() -> k True
+;;;   let cons = \p ps ->
+;;;     let k1 = make_candidate_cont (i,ps,k)
+;;;     divides i p k1
+;;;   match ps nil cons
+;;;
+;;; [] iL iH psL psH kL kH
+candidate:
     word .roots, .evac, .scav
 .code:
-    lda $1 ;il
-    ora $2 ;ih
-    beq .baseT ;i==0
-    sec
-    lda $1 ;il
-    sbc $3 ;pl
-    sta $1 ; (ok to save even if we branch to baseF)
-    lda $2 ;il
-    sbc $4 ;pl
-    bmi .baseF
-    sta $2
-    JUMP .code ; divides i' p k
-.baseT:
+    ;debug 'A'
+
+    ;; match ps nil cons
+    push_word_immediate .nil
+    push_word_immediate .cons
+
+    ;; copy_word_from_array 3, 0, cp
+    ;; jmp (cp)
+
+    ;; TODO: need to set fp even for data closures !
+    copy_word 3,fp ;ps
+    enter_fp ; k True
+
+.nil:
+    ;debug 'n'
     copy_word 5,fp ;k
     lda #True
     sta $1
     enter_fp ; k True
-.baseF:
-    copy_word 5,fp ;k
+.cons:
+    ;debug 'c'
+    ;; \p ps' -> let k1 = make_candidate_cont (i,ps,k) ...
+    ;; allocate continuation
+    lda #8
+    jsr alloc
+    ;; fill in closure
+    copy_code_pointer_to_heap0 candidate_cont.code
+    copy_word_local_to_heap 1, 2 ; i
+    pla ; ps'L
+    store_heap 4
+    pla ; ps'H
+    store_heap 5
+    copy_word_local_to_heap 5, 6 ; k
+    ;; setup args
+    ;; iL,iH already in 1,2
+    pla ; pL
+    sta 3
+    pla ; pH
+    sta 4
+    copy_word clo, 5
+    copy_code_pointer_to_local divides.static_closure, fp
+    JUMP divides.code ; divides i p k1
+.roots:
+    gc_root_at 2 ; ps
+    gc_root_at 4 ; k
+    rts
+.evac:
+    no_evacuate_because_static
+.scav:
+    impossible_scavenge_because_static
+.static_closure:
+    word .code
+
+;;; make_candidate_cont :: (Int,List Int,(Bool -> r)) -> Bool -> r
+;;; make_candidate_cont (i,ps,k) = \b -> if b then k False else candidate i ps k
+;;;
+;;; [. . iL iH psL psH kL kH] b
+candidate_cont:
+    word .roots, .evac, .scav
+.code:
+    ;debug 'B'
+    ;debug_hex_byte 1
+    lda 1 ; b
+    bne .bTrue
+    ;; candidate i ps k
+    ;debug 'f'
+    copy_word_from_frame 2, 1 ; i
+    copy_word_from_frame 4, 3 ; ps
+    copy_word_from_frame 6, 5 ; k
+    copy_code_pointer_to_local candidate.static_closure, fp
+    JUMP candidate.code
+.bTrue:
+    ;debug 't'
+    copy_word_from_frame 6, 2 ; k -> fp (using 2 as a temp)
+    copy_word 2, fp
     lda #False
     sta $1
     enter_fp ; k False
 .roots:
     rts ; no roots
+.evac:
+    evacuate 8
+.scav:
+    scavenge_cell_at 4 ; ps
+    scavenge_cell_at 6 ; k
+    scavenge_done 8
+
+
+;;; divides :: Int -> Int -> (Bool -> r) -> r
+;;; divides i p k =
+;;;   if i == 0 then k True else let i' = i-p in if i' < 0 then k False else divides i' p k
+;;;
+;;; [] iL iH pL pH kL kH
+divides:
+    word .roots, .evac, .scav
+.code:
+    ;debug 'D'
+    ;debug_decimal_word 1
+    ;debug '/'
+    ;debug_decimal_word 3
+    ;panic '*'
+
+    lda $1 ;iL
+    ora $2 ;iH
+    beq .baseT ;i==0
+    sec
+    lda $1 ;iL
+    sbc $3 ;pL
+    sta $1 ;i'L (ok to save even if we branch to baseF)
+    lda $2 ;iL
+    sbc $4 ;pL
+    bmi .baseF
+    sta $2 ; i'H
+    JUMP .code ; divides i' p k
+.baseT:
+    ;debug 't'
+    copy_word 5,fp ;k
+    lda #True
+    sta $1
+    enter_fp ; k True
+.baseF:
+    ;debug 'f'
+    copy_word 5,fp ;k
+    lda #False
+    sta $1
+    enter_fp ; k False
+.roots:
+    gc_root_at 4 ; k
+    rts
 .evac:
     no_evacuate_because_static
 .scav:
