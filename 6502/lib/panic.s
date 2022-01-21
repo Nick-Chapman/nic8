da382ed57fcabf865cd05fe4bae790ebcd75b59c
Nick Chapman <nic@amadido.co.uk>
2022-01-21 10:45:14 +0000

    tidy panic

--- 6502/lib/panic.s
+++ 6502/lib/panic.s
@@ -1,18 +1,14 @@
 
-panic1: macro CHAR
-    print_char '!'
-    print_char \CHAR
-    jmp panic_flush_spin
-endmacro
+;;; panic macro
 
 panic: macro STRING
     print_char '!'
-    ;newline
     print_string \STRING
-    jmp panic_flush_spin
+    jmp panic.flush_spin
 endmacro
 
-panic_flush_spin:
+panic:
+.flush_spin:
     lda g_selected_screen
     jsr screen_flush_sub
 .spin
