3b9f380a1a9fec7245829beea33d5ee29aa4531c
Nick Chapman <nic@amadido.co.uk>
2022-01-21 13:13:02 +0000

    migrate pre-serial

--- 6502/app/pre-serial.s
+++ 6502/app/pre-serial.s
@@ -10,7 +10,7 @@
     org $8000
 
     include via.s
-    include ticks.s
+    include interrupts.s
     include sound.s
     include lcd.s
     include screen.s
@@ -33,37 +33,10 @@ NUM_SCREENS = 4
 g_screen_pointers = $80
 g_screens = $200 ; 4 * 32 bytes
 
-nmi:
-    pha
-    lda g_nmi_blocked
-    bne .done
-    lda #25 ; debounce time
-    sta g_nmi_blocked
-    inc g_nmi_count
-.done:
-    pla
-    rti
-
-irq: ; copy & extend version in ticks.s
-    pha
-    bit via.T1CL ; acknowledge interrupt
-    inc g_ticks
-    lda g_nmi_blocked
-    beq .done
-    dec g_nmi_blocked
-.done:
-    pla
-    rti
-
-init_nmi:
-    stz g_nmi_blocked
-    stz g_nmi_count
-    rts
-
 reset_main:
     jsr via.init
     jsr init_ticks
-    jsr init_nmi
+    jsr init_nmi_irq
     jsr sound.init ; silence
     jsr lcd.init
     jsr lcd.clear_display
