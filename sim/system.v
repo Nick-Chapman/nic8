0bbdbcdb186febc6a2058d5ce2799251965dcdc3
Nick Chapman <nic@amadido.co.uk>
2022-08-29 08:49:12 +0100

    sim: reorder/cleanup, use verilog tasks

--- sim/system.v
+++ sim/system.v
@@ -23,8 +23,7 @@ module main;
       $readmemh(prog, ram);
    end
 
-   reg       clk;
-   initial clk = 1;
+   reg clk = 1;
    always #5 clk = ~clk;
 
    int ticks = 0;
@@ -32,19 +31,18 @@ module main;
 
    always @(clk) if (ticks >= steps) $finish();
 
-   int lines = 0;
-   //always @(clk) #1 begin
-   always @(posedge clk) #1 begin
-   //always @(qreg) #1 begin
-      //if (lines % 10 == 0) begin
-      if (lines == 0) begin
-         $display("------------------------------------------------------------");
-         $display("ticks(edge)  PC AR BR XR IR  MEAX IPAXBMQ  i j  OUT abus/dbus");
-         $display("------------------------------------------------------------");
-      end
-      lines++;
-      $display("%6d(%s)  %2h %2h %2h %2h %2h |%b%b%b%b|%b%b%b%b%b%b%b| %b %b {%03d}  %2h/%2h"
-               ,ticks,(clk?"pos":"neg")
+   initial printBar;
+   always @(posedge clk) #1 printStatus;
+
+   task printBar;
+      $display("------------------------------------------------------------");
+      $display("time: ticks(^)   PC AR BR XR IR  MEAX IPAXBMQ  i j  OUT abus/dbus");
+      $display("------------------------------------------------------------");
+   endtask
+
+   task printStatus;
+      $display("%4d: %4d(%s)  %2h %2h %2h %2h %2h |%b%b%b%b|%b%b%b%b%b%b%b| %b %b {%03d}  %2h/%2h"
+               ,$time,ticks,(clk?"pos":"neg")
                ,pc,areg,breg,xreg,ir
                ,provideMem,provideAlu,provideA,provideX
                ,loadIR,loadPC,loadA,loadX,loadB,storeMem,doOut
@@ -52,51 +50,29 @@ module main;
                ,qreg
                ,abus,dbus
                );
-   end
+   endtask
 
-   reg [7:0] pc;
-   reg [7:0] areg;
+   reg [7:0] pc = 0;
+   reg [7:0] areg = 0;
    reg [7:0] breg;
    reg [7:0] xreg;
-   reg [7:0] ir;
+   reg [7:0] ir = 0;
    reg [7:0] qreg;
-
-   initial pc = 0;
-   initial areg = 0;
-   initial breg = 0;
-//   initial xreg = 0;
-   initial ir = 0;
-//   initial qreg = 0;
 
    always @(posedge clk) if (loadPC && jumpControl) pc <= abus;
    always @(posedge clk) if (immediate) pc <= pc + 1;
-
    always @(posedge clk) if (loadA) areg <= dbus;
    always @(posedge clk) if (loadB) breg <= dbus;
    always @(posedge clk) if (loadX) xreg <= dbus;
    always @(posedge clk) if (doOut) qreg <= dbus;
-
    always @(posedge clk) ir <= loadIR ? ram[abus] : 0;
-
    always @(posedge clk) if (storeMem) ram[abus] = dbus;
-
-   wire [7:0] dbus;
-   assign dbus = provideMem ? ram[abus] : 'z;
-   assign dbus = provideA ? areg : 'z;
-   assign dbus = provideX ? xreg : 'z;
-   assign dbus = provideAlu ? aluOut : 'z; //TODO
-
-   wire [7:0] aluOut = areg + breg;
-
-   wire [7:0] abus = immediate?pc:xreg;
 
    wire      bit7, bit6;
    wire [1:0] source;
    wire [2:0] dest;
    wire       indexed;
    assign {bit7,bit6,source,dest,indexed} = ir;
-
-   wire       immediate = ~indexed;
 
    wire       provideMem = (source==0);
    wire       provideAlu = (source==1);
@@ -111,9 +87,20 @@ module main;
    wire       storeMem = (dest==5);
    wire       doOut = (dest==6);
 
+   wire       immediate = ~indexed;
    wire       unconditionalJump = bit6 && bit7;
 
    wire       jumpControl = unconditionalJump; //TEMP
    //wire       jumpControl = (jumpIfZero && aIsZero) || (jumpIfCarry && flagCarry) || unconditionalJump; //TODO
+
+   wire [7:0] aluOut = areg + breg;
+
+   wire [7:0] abus = immediate?pc:xreg;
+
+   wire [7:0] dbus;
+   assign dbus = provideMem ? ram[abus] : 'z;
+   assign dbus = provideA ? areg : 'z;
+   assign dbus = provideX ? xreg : 'z;
+   assign dbus = provideAlu ? aluOut : 'z; //TODO
 
 endmodule
