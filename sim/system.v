629d38635c6d9d101fae99e0ec3a92d8b0bfd74a
Nick Chapman <nic@amadido.co.uk>
2022-08-31 16:33:24 +0100

    sub: split out submodule for control logic

--- sim/system.v
+++ sim/system.v
@@ -57,38 +57,9 @@ module main;
 
    always @(posedge clk) if (storeMem) ram[abus] = dbus;
 
-   wire      bit7, bit6;
-   wire [1:0] source;
-   wire [2:0] dest;
-   wire       indexed;
-   assign {bit7,bit6,source,dest,indexed} = ir;
-
-   wire       provideMem = (source==0);
-   wire       provideAlu = (source==1);
-   wire       provideA = (source==2);
-   wire       provideX = (source==3);
-
-   wire       loadIR = (dest==0);
-   wire       loadPC = (dest==1);
-   wire       loadA = (dest==2);
-   wire       loadX = (dest==3);
-   wire       loadB = (dest==4);
-   wire       storeMem = (dest==5);
-   wire       doOut = (dest==6);
-
-   wire       immediate = ~indexed;
-
-   wire       aIsZero = (areg == 0);
-
-   wire       jumpIfZero = bit6;
-   wire       jumpIfCarry = bit7;
-   wire       unconditionalJump = bit6 && bit7;
-
-   wire       jumpControl = (jumpIfZero && aIsZero) || (jumpIfCarry && flagCarry) || unconditionalJump;
-
-   wire       doSubtract = bit6;
    wire [7:0] aluOut = doSubtract ? areg - breg : areg + breg;
    wire       carry = doSubtract ? !(breg > areg) : (areg + breg >= 256);
+   wire       aIsZero = (areg == 0);
 
    wire [7:0] abus = immediate?pc:xreg;
    wire [7:0] mbus = ram[abus];
@@ -99,16 +70,59 @@ module main;
    assign dbus = provideX ? xreg : 'z;
    assign dbus = provideAlu ? aluOut : 'z;
 
-   registers r
-     (clk,loadIR,loadPC,loadA,loadB,loadX,doOut,immediate,jumpControl,provideAlu,carry,dbus,abus,mbus,
-      ir,pc,areg,breg,xreg,qreg,flagCarry
-      );
+   wire [1:14] controlBits;
+
+   wire loadIR,loadPC,loadA,loadB,loadX,doOut,storeMem;
+   wire provideMem,provideA,provideX,provideAlu;
+   wire immediate,jumpControl,doSubtract;
+
+   assign {loadIR,loadPC,loadA,loadB,loadX,doOut,storeMem,
+           provideMem,provideA,provideX,provideAlu,
+           immediate,jumpControl,doSubtract
+           } = controlBits;
+
+   registers r (clk,loadIR,loadPC,loadA,loadB,loadX,doOut,immediate,jumpControl,provideAlu,carry,
+                dbus,abus,mbus,
+                ir,pc,areg,breg,xreg,qreg,flagCarry);
+
+   control c (ir,aIsZero,flagCarry,controlBits);
 
 endmodule
 
+
+module control (input [7:0] ir, input aIsZero, flagCarry, output [1:14] controlBits);
+   wire [1:14] controlBits =
+               {loadIR,loadPC,loadA,loadB,loadX,doOut,storeMem,
+                provideMem,provideA,provideX,provideAlu,
+                immediate,jumpControl,doSubtract};
+   wire bit7, bit6;
+   wire [1:0] source;
+   wire [2:0] dest;
+   wire indexed;
+   assign {bit7,bit6,source,dest,indexed} = ir;
+   wire provideMem = (source==0);
+   wire provideAlu = (source==1);
+   wire provideA = (source==2);
+   wire provideX = (source==3);
+   wire loadIR = (dest==0);
+   wire loadPC = (dest==1);
+   wire loadA = (dest==2);
+   wire loadX = (dest==3);
+   wire loadB = (dest==4);
+   wire storeMem = (dest==5);
+   wire doOut = (dest==6);
+   wire immediate = ~indexed;
+   wire jumpIfZero = bit6;
+   wire jumpIfCarry = bit7;
+   wire unconditionalJump = bit6 && bit7;
+   wire jumpControl = (jumpIfZero && aIsZero) || (jumpIfCarry && flagCarry) || unconditionalJump;
+   wire doSubtract = bit6;
+endmodule
+
+
 module registers
-  (input clk, loadIR, loadPC, loadA, loadB, loadX, doOut, immediate, jumpControl,provideAlu,carry,
-   input [7:0]  dbus,abus,mbus,
+  (input clk, loadIR, loadPC, loadA, loadB, loadX, doOut, immediate, jumpControl, provideAlu, carry,
+   input [7:0] dbus,abus,mbus,
    output reg [7:0] ir, pc, areg, breg, xreg, qreg,
    output reg [0:0] flagCarry
    );
